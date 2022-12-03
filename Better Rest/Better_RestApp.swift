dcdf7fe9974c03a2b9dc3811d48812447245260a
micahsteele <micahs66@gmail.com>
2022-12-03 14:50:47 -0500

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by Micah Steele on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
