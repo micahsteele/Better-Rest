54c2902a218dacf4254af1fb3d837c79bd921825
micahsteele <micahs66@gmail.com>
2022-12-05 22:10:42 -0500

    BetterRest

--- Better Rest/ContentView.swift
+++ Better Rest/ContentView.swift
@@ -5,17 +5,55 @@
 //  Created by Micah Steele on 12/3/22.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    @State private var coffeeAmount = 0
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                VStack {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    
+                    Text("How much sleep do you want")
+                        .font(.headline)
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 0...24, step: 0.25)
+                    
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper(coffeeAmount == 1 ? "\(coffeeAmount) cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 0...20)
+                    
+                }
+            }
+            .navigationTitle("Better Rest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+        }    
+    }
+    
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hours = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            
+        } catch {
+            // something went wrong
         }
-        .padding()
     }
 }
 
